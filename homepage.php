6c00e1f8f247bdb4a77424d246b00b850de3dc26
whuang15 <whhuang19@gmail.com>
2016-01-30 19:36:17 +0000

    new homepage

---
+++ homepage.php
@@ -0,0 +1,36 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Settle the Tab</title>
+  <meta name="description" content="Settle the Tab">
+
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Signika:400,600' rel='stylesheet' type='text/css'>
+
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/style.css">
+
+  <!--[if lt IE 9]>
+  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+
+</head>
+
+<body>
+  <!--<script src="js/scripts.js"></script>-->
+  <div id="header"><h1>Settle the Tab</h1></div>
+  <div class="border"></div>
+
+  <div class="container left">
+  <button class="button" id="login">Login</button>
+  </div>
+
+  <div class="container right">
+  <a href="register.php"><button class="button" id="reg">Register</button></a>
+  </div>
+
+</body>
+</html>

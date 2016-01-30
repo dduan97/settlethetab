81e7a4684612cb4c573679115cbfa6a83784ac04
whuang15 <whhuang19@gmail.com>
2016-01-30 17:04:59 +0000

    adding frontend

---
+++ index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Settle the Tab</title>
+  <meta name="description" content="Settle the Tab">
+
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Signika:400,600' rel='stylesheet' type='text/css'>
+
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/style.css">
+
+  <!--[if lt IE 9]>
+  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+
+</head>
+
+<body>
+  <!--<script src="js/scripts.js"></script>-->
+  <div id="header"><h1>Settle the Tab</h1></div>
+  <div class="border"></div>
+
+  <div class="container">
+  <button class="button" id="scan">Scan Receipt</button>
+  <button class="button" id="iou">Manual Charge</button>
+  </div>
+
+</body>
+</html>

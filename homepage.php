13a92edd90d114cc878da7831dcda86ef99f72d8
whuang15 <whhuang19@gmail.com>
2016-01-30 21:16:31 +0000

    new homepage

--- homepage.php
+++ homepage.php
@@ -5,6 +5,7 @@
   <meta charset="utf-8">
 
   <title>Settle the Tab</title>
+  <link rel="shortcut icon" href="SettletheTabIcon.png">
   <meta name="description" content="Settle the Tab">
 
   <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600' rel='stylesheet' type='text/css'>
@@ -21,15 +22,33 @@
 
 <body>
   <!--<script src="js/scripts.js"></script>-->
-  <div id="header"><h1>Settle the Tab</h1></div>
+  <div id="header"><img src="SettletheTabLogo.png"></div>
   <div class="border"></div>
 
+  <div class="block">
   <div class="container left">
   <button class="button" id="login">Login</button>
   </div>
 
   <div class="container right">
   <a href="register.php"><button class="button" id="reg">Register</button></a>
+  </div>
+  </div>
+
+  <div class="block">
+  <h1 id="about">About</h1>
+  <p><b>SCENARIO</b> &nbsp; You and 7 friends go out to dinner. You all order
+  dishes of varying prices and have a nice time. But when the check comes,
+  you are in a bit of a pickle. To your dismay, the waitress informs you
+  that <i>you may not split the check</i>. Glances shift around the table.
+  There's an awkward silence. There's no choice - you step up and
+  cover the entire meal. "You all owe me," you announce to your friends.
+  The order is incredibly complicated, and to make it worse, some have
+  cash, some have credit, and no one seems to have the right amount
+  of change. <b>What do you do?</b></p>
+
+  <p>Solve mealtime crises the right way with <b><a href="index.php">
+  Settle the Tab</a></b>.</p>
   </div>
 
 </body>

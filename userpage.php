c06099d4a660b246e31df83650cd05275c76a347
whuang15 <whhuang19@gmail.com>
2016-01-31 00:10:28 +0000

    adding user page

---
+++ userpage.php
@@ -0,0 +1,44 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>User Page | Settle the Tab</title>
+  <link rel="shortcut icon" href="SettletheTabIcon.png">
+  <meta name="description" content="Settle the Tab">
+
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Signika:400,600' rel='stylesheet' type='text/css'>
+
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/style.css">
+
+  <!--[if lt IE 9]>
+  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+
+</head>
+
+<body>
+  <!--<script src="js/scripts.js"></script>-->
+  <div id="header"><a href="index.php"><img src="SettletheTabLogo.png"></a></div>
+  <div class="border"></div>
+
+  <div class="block">
+  <div class="container" style="float: left;">
+  <a href="scan.php"><button class="button green" id="scan">Scan Receipt</button></a>
+  <button class="button red" id="charge">Manual Charge</button>
+  </div>
+
+  <div class="container" id="friends">
+  <h2>FRIENDS</h2>
+  <div class="content">
+  <h3>NET BALANCE:</h3>
+  </div>
+  </div>
+  </div>
+
+
+</body>
+</html>
